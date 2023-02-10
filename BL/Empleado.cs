34d30e14e49b892f4ff5390a8f84ffd3f28f8fef
Efrain Ignacio <efrainignaciohernandez@gmail.com>
2023-02-09 18:55:37 -0600

    Add project files.

---
+++ BL/Empleado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Empleado
+    {
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.EIgnacioSDIEntities context=new DL.EIgnacioSDIEntities())
+                {
+                    var query=context.GetAll().ToList();
+                    result.ObjectList = new List<object>();
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return result;
+        }
+    }
+}
